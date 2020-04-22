4164ef07f71e33f7290dc3b08b9232c9741e5122
Farra <ainfarra11@gmail.com>
2020-04-22 08:07:53 +0800

    Create TodoModels

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -2,7 +2,16 @@
 
 import 'package:exercise2/models/task.dart';
 
+import 'task.dart';
+import 'task.dart';
+
 class Todo {
-  String title;
+  Task title;
   List<Task> items;
+
+  Todo(this.title,this.items);
+  Todo.copy(Todo from)  
+  : this(Task.copy(from.title), [ ...from.items]);
+
+
 }
